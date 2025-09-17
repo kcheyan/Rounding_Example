8075c3527760b8633ade2984f390233f79151d60
kcheyan <kathy.cheyan@gmail.com>
2025-09-17 10:30:49 -0400

    Create Rounding_Example.java

---
+++ Rounding_Example.java
@@ -0,0 +1,18 @@
+public class NearestInt
+{
+    public static void main(String[] args)
+    {
+        double number = 5.0 / 3;
+        int nearestInt = (int) (number + 0.5);
+        System.out.println("5.0/3 = " + number);
+        System.out.println("5/3 truncated: " + (int) number);
+        System.out.println("5.0/3 rounded to nearest int: " + nearestInt);
+        double negNumber = -number;
+        int nearestNegInt = (int) (negNumber - 0.5);
+        System.out.println(
+                "-5.0/3 rounded to nearest negative int: " + nearestNegInt);
+
+        // Print the result of rounding (number + 2.3) to the nearest int.
+        System.out.println((int) ((number + 2.3) + 0.5));
+
+    }
